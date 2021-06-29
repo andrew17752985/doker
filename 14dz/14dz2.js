a3e7f6f8c8decfe0d27cb0675779abe521a0b08c
andrew17752985 <mr.androvich55555@gmail.com>
2021-06-29 21:28:20 +0200

    otake

--- 14dz/14dz2.js
+++ 14dz/14dz2.js
@@ -1,6 +1,38 @@
 document.getElementById("zd4").addEventListener("click", executeTask4);
 
+const tests = [
+    {
+        prompt:'2+2',
+        answer:'4'
+    },
+    {
+        prompt:'1+1',
+        answer:'2'
+    }, {
+        prompt:'2+1',
+        answer:'3'
+    }, {
+        prompt:'2+5',
+        answer:'7'
+    }, {
+        prompt:'2+9',
+        answer:'11'
+    },
+];
+
+let score = 0;
+
+
+
 function executeTask4(){
     alert('завдання чотири');
+    for (let i=0; i<tests.length; i++){
+        let result = prompt(tests[i].prompt);
+        if(result==tests[i].answer){
+            score = score +1;
+        }
+    }
+    alert(`ви відповіли на ${score} балів `);
     
+   
 }
--- 14dz/14dz4.js
+++ 14dz/14dz4.js
@@ -1,5 +1,23 @@
 document.getElementById("zd6").addEventListener("click", executeTask6);
 
+const chisla = {
+    '1':'!', 
+    '2':'@',
+    '3':'#',
+    '4':'$',
+    '5':'%',
+    '6':'^',
+    '7':'&',
+    '8':'*',
+    '9':'(',
+    '0':')',
+    '-':'_',
+    '=':'+',
+}
+
 function executeTask6(){
     alert('завдання шість');
+   let sumphol = prompt('нажимай на цифри братан');
+   alert(chisla[sumphol]);
+   
 }
